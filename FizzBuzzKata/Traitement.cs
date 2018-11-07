4ac901b4954a191de7816b500dc98f42d6e2516b
houssem aouadi <houssemjean22@live.fr>
2018-11-07 16:37:05 +0100

    validation

---
+++ FizzBuzzKata/Traitement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzzKata;
+
+namespace FizzBuzzKata
+{
+    public static class Traitement
+    {
+        public static string getNumbers() {
+            string result = String.Empty;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                        result += " " + "FizzBuzz";
+                    else
+                        result += " " + "Fizz";
+                }
+                else
+                    if (i % 5 == 0)
+                    result += " " + "Buzz";
+                else
+                    result += " " + i.ToString();
+            }
+            return result.Trim();
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzzKata;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestContainsFizz()
+        {
+            string numbers = Traitement.getNumbers();
+            Assert.IsTrue(numbers.Contains("Fizz"));
+        }
+
+        [TestMethod]
+        public void TestContainsBuzz()
+        {
+            string numbers = Traitement.getNumbers();
+            Assert.IsTrue(numbers.Contains("Buzz"));
+        }
+
+        [TestMethod]
+        public void TestContainsFizzBuzz()
+        {
+            string numbers = Traitement.getNumbers();
+            Assert.IsTrue(numbers.Contains("FizzBuzz"));
+        }
+
+        [TestMethod]
+        public void TestFizzBuzzIsOK()
+        {
+            string numbers = Traitement.getNumbers();
+            bool fail = false;
+            string[] numbersArray = numbers.Split(new string[] { " " },StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < 100; i++) {
+                if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0) {
+                    if (numbersArray[i] != "FizzBuzz") fail = true;
+                }
+            }
+            Assert.IsFalse(fail);
+        }
+
+        [TestMethod]
+        public void TestFizzIsOK()
+        {
+            string numbers = Traitement.getNumbers();
+            bool fail = false;
+            string[] numbersArray = numbers.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < 100; i++)
+            {
+                if ((i + 1) % 3 == 0 && (i + 1) % 5 != 0)
+                {
+                    if (numbersArray[i] != "Fizz") fail = true;
+                }
+            }
+            Assert.IsFalse(fail);
+        }
+
+        [TestMethod]
+        public void TestBuzzIsOK()
+        {
+            string numbers = Traitement.getNumbers();
+            bool fail = false;
+            string[] numbersArray = numbers.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < 100; i++)
+            {
+                if ((i + 1) % 3 != 0 && (i + 1) % 5 == 0)
+                {
+                    if (numbersArray[i] != "Buzz") fail = true;
+                }
+            }
+            Assert.IsFalse(fail);
+        }
+
+
+
+
+
+
+
+
+    }
+}
